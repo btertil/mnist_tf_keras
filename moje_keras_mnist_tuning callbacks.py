62e9ddd7fd9fd25e187fc0e4e12b9ed32de7eb89
Bartosz Tertil <bartosz.tertil@gmail.com>
2021-10-16 00:03:16 +0200

    poprawa learning rate schedulera: mniejszy kick

--- moje_keras_mnist_tuning callbacks.py
+++ moje_keras_mnist_tuning callbacks.py
@@ -102,7 +102,7 @@ def lr_scheduler(epoch, old_lr):
     if epoch < 6:
         learning_rate = old_lr
     elif epoch == 50 or epoch == 100:
-        learning_rate = base_learning_rate_global
+        learning_rate = base_learning_rate_global * 0.33
     elif epoch == 51 or epoch == 101:
         learning_rate = old_lr * 0.25
     else:
